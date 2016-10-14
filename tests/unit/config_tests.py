84d487e07eaab3e9c782b84438d62799b2837a75
Lucas Tadeu Teixeira <ltadeu@br.ibm.com>
2016-10-14 11:41:53 -0300

    Add ConfigParser.parse_argument_list unit tests

---
+++ tests/unit/config_tests.py
@@ -0,0 +1,49 @@
+from nose.tools import eq_
+from nose_parameterized import parameterized
+
+
+from lib.config import ConfigParser
+
+
+import unittest
+
+
+class TestConfigParser(unittest.TestCase):
+
+    @parameterized.expand([
+        ('--config-file=foo', 'config_file', 'foo'),
+        ('--packages=foo', 'packages', ['foo']),
+        ('--log-file=foo', 'log_file', 'foo'),
+        ('--verbose', 'verbose', True),
+        ('--result-dir=foo', 'result_dir', 'foo'),
+        ('--repositories-path=foo', 'repositories_path', 'foo'),
+        ('--keep-builddir', 'keep_builddir', True),
+        ('--build-versions-repository-url=foo', 'build_versions_repository_url', 'foo'),
+        ('--build-version=foo', 'build_version', 'foo'),
+    ])
+    def test_parse_arguments_list_WithLongArgument_ShouldParseArgumentValue(self, argument, key, expected):
+        cfg = ConfigParser()
+
+        result_dict = cfg.parse_arguments_list([argument])
+        value = result_dict.get(key)
+
+        eq_(value, expected)
+
+    @parameterized.expand([
+        ('config_file', './config.yaml'),
+        ('packages', None),
+        ('log_file', '/var/log/host-os/builds.log'),
+        ('verbose', False),
+        ('result_dir', './result'),
+        ('repositories_path', '/var/lib/host-os/repositories'),
+        ('keep_builddir', False),
+        ('build_versions_repository_url', None),
+        ('build_version', None),
+    ])
+    def test_parse_arguments_list_WithoutArgument_ShouldUseDefaultValue(self, key, expected):
+        cfg = ConfigParser()
+
+        result_dict = cfg.parse_arguments_list([])
+        value = result_dict.get(key)
+
+        eq_(value, expected)
