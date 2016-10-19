open-power-host-os/builds

commit 8e32920e2aaa62c3093de99bcd22f00135db2c87

Catch exception from missing package files in target branch
Fixes #54

lib/package.py
@@ -159,6 +159,10 @@ class Package(object):
                 print("%s: Loaded package metadata successfully" % self.name)
         except TypeError:
             raise exception.PackageDescriptorError(package=self.name)
+        except IOError:
+            raise exception.PackageDescriptorError(
+                "Failed to open %(package)s's YAML descriptor",
+                package=self.name)
 
     def _setup_repository(self, dest=None, branch=None):
         self.repository = repository.Repo(package_name=self.name,

commit 1b0045fef2328b0c9274abdb87aa925ad937748e

Fix exception handling in package.py
The issue was introduced by: "8e32920 Catch exception from missing
package files in target branch"

lib/package.py
@@ -161,8 +161,7 @@ class Package(object):
             raise exception.PackageDescriptorError(package=self.name)
         except IOError:
             raise exception.PackageDescriptorError(
-                "Failed to open %(package)s's YAML descriptor",
-                package=self.name)
+                "Failed to open %s's YAML descriptor" % self.name)
 
     def _setup_repository(self, dest=None, branch=None):
         self.repository = repository.Repo(package_name=self.name,