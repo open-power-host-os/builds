eaaca0ffcf51c71022ed6c14d9e2a06781d98a61
Olav Philipp Henschel <olavph@linux.vnet.ibm.com>
2016-11-03 09:22:29 -0200

    Minimize try blocks when creating versions repository
    Also, rename dirname variable.

--- lib/utils.py
+++ lib/utils.py
@@ -50,12 +50,12 @@ def setup_versions_repository(config):
     Clone and checkout the versions repository and halt execution if
     anything fails.
     """
+    path, dir_name = os.path.split(
+        config.get('default').get('build_versions_repo_dir'))
+    url = config.get('default').get('build_versions_repository_url')
+    branch = config.get('default').get('build_version')
     try:
-        path, dirname = os.path.split(
-            config.get('default').get('build_versions_repo_dir'))
-        repository.Repo(
-            dirname, config.get('default').get('build_versions_repository_url'),
-            path, config.get('default').get('build_version'))
+        repository.Repo(dir_name, url, path, branch)
     except exception.RepositoryError as exc:
         LOG.exception("Failed to checkout versions repository")
         sys.exit(exc.errno)
