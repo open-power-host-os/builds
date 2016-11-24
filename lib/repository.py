open-power-host-os/builds

commit f035b73dc2467d7bd2c99c76b4643ba09d3be2af

Add HTTP Proxy configuration support

lib/repository.py
@@ -20,8 +20,11 @@ import utils
 import git
 import gitdb
 
+from lib import config
 from lib import exception
 
+
+CONF = config.get_config().CONF
 LOG = logging.getLogger(__name__)
 
 
@@ -43,20 +46,33 @@ def get_git_repository(name, remote_repo_url, parent_dir_path):
     if os.path.exists(repo_path):
         return GitRepository(repo_path)
     else:
-        return GitRepository.clone_from(remote_repo_url, repo_path)
+        return GitRepository.clone_from(remote_repo_url,
+                                        repo_path,
+                                        proxy=CONF.get('http_proxy'))
+
 
 class GitRepository(git.Repo):
 
     @classmethod
-    def clone_from(cls, remote_repo_url, repo_path, *args, **kwargs):
+    def clone_from(cls, remote_repo_url, repo_path, proxy=None, *args, **kwargs):
         """
         Clone a repository from a remote URL into a local path.
         """
         LOG.info("Cloning repository from '%s' into '%s'" %
                  (remote_repo_url, repo_path))
         try:
-            return super(GitRepository, cls).clone_from(
-                remote_repo_url, repo_path, *args, **kwargs)
+            if proxy:
+                git_cmd = git.cmd.Git()
+                git_cmd.execute(['git',
+                                 '-c',
+                                 "http.proxy='{}'".format(proxy),
+                                 'clone',
+                                 remote_repo_url,
+                                 repo_path])
+                return GitRepository(repo_path)
+            else:
+                return super(GitRepository, cls).clone_from(
+                    remote_repo_url, repo_path, *args, **kwargs)
         except git.exc.GitCommandError:
             message = "Failed to clone repository"
             LOG.exception(message)

lib/config.py
@@ -128,6 +128,8 @@ class ConfigParser(object):
         self.parser.add_argument('--log-size',
                                  help='Size in bytes above which the log file '
                                  'should rotate', type=int)
+        self.parser.add_argument('--http-proxy',
+                                 help='HTTP proxy URL')
         self.parser.add_argument('--push-repo-url',
                                  help='URL of the repository used for pushing')
         self.parser.add_argument('--push-repo-branch',

lib/utils.py
@@ -27,6 +27,12 @@ from lib import repository
 LOG = logging.getLogger(__name__)
 
 
+def set_http_proxy_env(proxy):
+    LOG.info('Setting up http proxy: {}'.format(proxy))
+    os.environ['https_proxy'] = proxy
+    os.environ['http_proxy'] = proxy
+
+
 def setup_default_config():
     """
     Setup the script environment. Parse configurations, setup logging
@@ -41,6 +47,10 @@ def setup_default_config():
     log_helper.LogHelper(logfile=CONF.get('default').get('log_file'),
                          verbose=CONF.get('default').get('verbose'),
                          rotate_size=CONF.get('default').get('log_size'))
+
+    proxy = CONF.get('http_proxy')
+    if proxy:
+        set_http_proxy_env(proxy)
 
     return CONF
 

commit d17c3104a9a61adc797011ba5224f3ae5fdb3226

Fix inappropriate call to CONF.get_config in repository.py
This bug was introduced in commit
f035b73dc2467d7bd2c99c76b4643ba09d3be2af.

lib/repository.py
@@ -24,7 +24,6 @@ from lib import config
 from lib import exception
 
 
-CONF = config.get_config().CONF
 LOG = logging.getLogger(__name__)
 
 
@@ -46,6 +45,7 @@ def get_git_repository(name, remote_repo_url, parent_dir_path):
     if os.path.exists(repo_path):
         return GitRepository(repo_path)
     else:
+        CONF = config.get_config().CONF
         return GitRepository.clone_from(remote_repo_url,
                                         repo_path,
                                         proxy=CONF.get('http_proxy'))