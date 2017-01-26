open-power-host-os/builds

commit 0839c75a5c7f528e7ca090d0542b04e843f20902

Fix changelog having only one character per line

lib/rpm_package.py
@@ -104,7 +104,7 @@ class SpecFile(object):
         self.write_content()
 
     def bump_release(self, log, user_name, user_email):
-        comment = "\n".join(['- ' + l for l in log])
+        comment = "\n".join(['- ' + l for l in log.splitlines()])
         user_string = "%(user_name)s <%(user_email)s>" % locals()
         cmd = "rpmdev-bumpspec -c '%s' -u '%s' %s" % (
             comment, user_string, self.path)

commit 8973a5c6b31fa0ec8fde397f97ad6b907bffe88d

Fix upgrade-versions command
When migrating from the old yaml sources format to the new one, the
commit that modified the upgrade_versions module
(1161796bbb0408eb981fbebf25c79bdd107b3f65) was incomplete, preventing
the spec release bumps.

The spec release bump and git commit logging were broken since commit
0839c75a5c7f528e7ca090d0542b04e843f20902. Only version updates were
working, but because the release bump was not even happening, the issue
was not observed.

lib/rpm_package.py
@@ -104,7 +104,7 @@ class SpecFile(object):
         self.write_content()
 
     def bump_release(self, change_log_lines, user_name, user_email):
-        comment = "\n".join(['- ' + l for l in change_log_lines.splitlines()])
+        comment = "\n".join(['- ' + l for l in change_log_lines])
         user_string = "%(user_name)s <%(user_email)s>" % locals()
         cmd = "rpmdev-bumpspec -c '%s' -u '%s' %s" % (
             comment, user_string, self.path)

tools/upgrade_versions.py
@@ -83,7 +83,7 @@ class Version(object):
         LOG.info("%s: Current version: %s" % (self.pkg, self.pkg.version))
 
     def update(self, user_name, user_email):
-        pkg = copy.copy(self.pkg)
+        pkg = copy.deepcopy(self.pkg)
         pkg.sources[0]["git"]["commit_id"] = None
         pkg.download_files(recurse=False)
         newest_commit_id = pkg.sources[0]["git"]["repo"].head.commit.hexsha
@@ -111,16 +111,23 @@ class Version(object):
         pkg.spec_file.update_prerelease_tag(self._repo_prerelease)
         self._bump_release(pkg, change_log_header, user_name, user_email)
 
-    def _bump_release(self, pkg, change_log_lines=None, user_name=None,
+    def _bump_release(self, pkg, change_log_header=None, user_name=None,
                       user_email=None):
         LOG.info("%s: Bumping release" % self.pkg)
+        change_log_lines = []
+        if change_log_header is not None:
+            change_log_lines.append(change_log_header)
 
-        if self.pkg.commit_id:
+        old_commit_id = self.pkg.sources[0]["git"]["commit_id"]
+        if old_commit_id:
+            new_source = pkg.sources[0]["git"]
+            new_commit_id = new_source["commit_id"]
             LOG.info("Updating package %s from %s to %s" % (
-                self.pkg.name, self.pkg.commit_id, pkg.commit_id))
-            change_log_lines = _get_git_log(pkg.repository, self.pkg.commit_id)
-            _sed_yaml_descriptor(self.pkg.package_file, self.pkg.commit_id,
-                                 pkg.commit_id)
+                self.pkg.name, old_commit_id, new_commit_id))
+            change_log_lines += _get_git_log(
+                new_source["repo"], old_commit_id)
+            _sed_yaml_descriptor(
+                self.pkg.package_file, old_commit_id, new_commit_id)
 
         if change_log_lines:
             assert user_name is not None