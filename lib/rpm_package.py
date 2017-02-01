open-power-host-os/builds

commit e7562dd3e7f0abd6045db16024fe14eb25d738b7

Update commit ID in rpm specification files when updating commit ID in yaml files
This is done in upgrade_versions.py script.

lib/rpm_package.py
@@ -119,6 +119,9 @@ class SpecFile(object):
         LOG.info("Updated '%s' prerelease tag to: %s"
                  % (self.path, new_prerelease))
 
+    def update_commit_id(self, old_commit_id, new_commit_id):
+        utils.replace_str_in_file(self.path, old_commit_id, new_commit_id)
+
     def _replace_macro_definition(self, macro_name, replacement):
         """
         Updates the file content cache, replacing the macro value.

lib/utils.py
@@ -137,3 +137,14 @@ def create_directory(directory):
     """
     if not os.path.isdir(directory):
         os.makedirs(directory)
+
+
+def replace_str_in_file(file_path, search, replacement):
+
+    lines = []
+    with file(file_path, "r") as f:
+        lines = f.readlines()
+    with file(file_path, "w") as f:
+        for line in lines:
+            line = line.replace(search, replacement)
+            f.write(line)

tools/upgrade_versions.py
@@ -26,6 +26,7 @@ from lib import exception
 from lib import packages_manager
 from lib import repository
 from lib import rpm_package
+from lib.utils import replace_str_in_file
 from lib.versions_repository import setup_versions_repository
 
 
@@ -49,16 +50,6 @@ PACKAGES = [
 
 # prerelease strings supported as last element in the version regex
 PRERELEASE_TERMS = ['rc']
-
-
-def _sed_yaml_descriptor(yamlfile, old_commit, new_commit):
-    lines = []
-    with file(yamlfile, "r") as f:
-        lines = f.readlines()
-    with file(yamlfile, "w") as f:
-        for line in lines:
-            line = line.replace(old_commit, new_commit)
-            f.write(line)
 
 
 def _get_git_log(repo, since_id):
@@ -130,8 +121,9 @@ class Version(object):
                 self.pkg.name, old_commit_id, new_commit_id))
             change_log_lines += _get_git_log(
                 new_source["repo"], old_commit_id)
-            _sed_yaml_descriptor(
+            replace_str_in_file(
                 self.pkg.package_file, old_commit_id, new_commit_id)
+            pkg.spec_file.replace_commit_id(old_commit_id, new_commit_id)
 
         if change_log_lines:
             assert user_name is not None

commit a4df41e2a251ff0336b0f65213e651b6372c6a2a

Fix method name in SpecFile class
Rename SpecFile's update_commit_id method to replace_commit_id to match
the call from upgrade_versions module.
Regression introduced by commit e7562dd3e7f0abd6045db16024fe14eb25d738b7

lib/rpm_package.py
@@ -119,7 +119,7 @@ class SpecFile(object):
         LOG.info("Updated '%s' prerelease tag to: %s"
                  % (self.path, new_prerelease))
 
-    def update_commit_id(self, old_commit_id, new_commit_id):
+    def replace_commit_id(self, old_commit_id, new_commit_id):
         utils.replace_str_in_file(self.path, old_commit_id, new_commit_id)
 
     def _replace_macro_definition(self, macro_name, replacement):