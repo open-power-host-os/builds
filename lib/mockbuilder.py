open-power-host-os/builds

commit f20d7166870e918c3976fb6846feca688ea4df44

Archive build artifacts to a package specific directory
This makes it straightforward to know which package generated which
RPMs. It also needed to know whether a package already has results from
previous builds. Before, we had no permanent information to link an RPM
to a package name after the Package object was destroyed.

lib/mockbuilder.py
@@ -17,6 +17,7 @@ from functools import partial
 
 import datetime
 import logging
+import glob
 import os
 import shutil
 
@@ -36,7 +37,6 @@ class Mock(build_system.PackageBuilder):
         super(Mock, self).__init__()
         binary_file = CONF.get('default').get('mock_binary')
         extra_args = CONF.get('default').get('mock_args')
-        self.result_dir = CONF.get('default').get('result_dir')
         self.build_dir = None
         self.archive = None
         self.timestamp = datetime.datetime.now().isoformat()
@@ -129,7 +129,9 @@ class Mock(build_system.PackageBuilder):
             cmd = self.common_mock_args
             install = " --install"
             for dep in package.build_dependencies:
-                install = " ".join([install, " ".join(dep.result_packages)])
+                dep_rpms_glob = os.path.join(dep.build_results_dir, "*.rpm")
+                rpm_files = glob.iglob(dep_rpms_glob)
+                install = " ".join([install, " ".join(rpm_files)])
 
             cmd = cmd + install
             LOG.info("%s: Installing dependencies on chroot" % package.name)
@@ -145,18 +147,18 @@ class Mock(build_system.PackageBuilder):
         shutil.rmtree(self.build_dir)
 
     def _save_rpm(self, package):
-        if not os.path.exists(self.result_dir):
-            LOG.info("Creating directory to store RPM at %s " %
-                     self.result_dir)
-            os.makedirs(self.result_dir)
-            os.chmod(self.result_dir, 0777)
+        if not os.path.exists(package.build_results_dir):
+            LOG.info("Creating directory to store RPMs at %s " %
+                     package.build_results_dir)
+            os.makedirs(package.build_results_dir)
+            os.chmod(package.build_results_dir, 0777)
 
-        LOG.info("%s: Saving RPMs at %s" % (package.name, self.result_dir))
+        LOG.info("%s: Saving RPMs at %s"
+                 % (package.name, package.build_results_dir))
         for f in os.listdir(self.build_dir):
             if f.endswith(".rpm") and not f.endswith(".src.rpm"):
-                LOG.info("Saving %s at result directory %s" % (f,
-                         self.result_dir))
+                LOG.info("Saving %s at result directory %s" % (
+                    f, package.build_results_dir))
                 orig = os.path.join(self.build_dir, f)
-                dest = os.path.join(self.result_dir, f)
+                dest = os.path.join(package.build_results_dir, f)
                 shutil.move(orig, dest)
-                package.result_packages.append(dest)

lib/package.py
@@ -56,7 +56,8 @@ class Package(object):
         self.download_source = None
         self.install_dependencies = []
         self.build_dependencies = []
-        self.result_packages = []
+        self.build_results_dir = os.path.join(
+            CONF.get('default').get('result_dir'), self.name)
         self.sources = []
         self.repository = None
         self.build_files = None

commit 67050ab557fe446fab156cb01a68020ca245265c

Remove build_results_dir attribute from Package class
This attribute is inferred from an option that is only available in the
build-package command and so it breaks the upgrade-versions command.

Since the only user of this attribute is the Mock class, that copies
the actual build results to this directory, it makes sense to create
this path there.

Regression introduced in commit f20d7166870e918c3976fb6846feca688ea4df44

lib/mockbuilder.py
@@ -37,6 +37,7 @@ class Mock(build_system.PackageBuilder):
         binary_file = CONF.get('default').get('mock_binary')
         extra_args = CONF.get('default').get('mock_args')
         self.build_dir = None
+        self.build_results_dir = CONF.get('default').get('result_dir')
         self.archive = None
         self.timestamp = datetime.datetime.now().isoformat()
         self.common_mock_args = (
@@ -184,4 +185,6 @@ class Mock(build_system.PackageBuilder):
         Args:
             package(Package): package whose result files will be copied
         """
-        self._copy_rpms(package.build_cache_dir, package.build_results_dir)
+        package_build_results_dir = os.path.join(
+            self.build_results_dir, package.name)
+        self._copy_rpms(package.build_cache_dir, package_build_results_dir)

lib/package.py
@@ -68,8 +68,6 @@ class Package(object):
         self.install_dependencies = []
         self.build_dependencies = []
         self.build_cache_dir = os.path.join(BUILD_CACHE_DIR, self.name)
-        self.build_results_dir = os.path.join(
-            CONF.get('default').get('result_dir'), self.name)
         self.sources = []
         self.repository = None
         self.build_files = None