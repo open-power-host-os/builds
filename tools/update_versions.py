open-power-host-os/builds

commit 5a2524a81a65cedd6a3731ac1a82793eaad9e10b

Rename upgrade-versions command line subcommand to update-versions
'update' is the terminology used in the rest of the UI and code.

Also, rename upgrade_versions attribute in config file to update_versions.

tools/update_versions.py
@@ -225,15 +225,15 @@ def push_packages_head_commit(
 
 def run(CONF):
     versions_repo = setup_versions_repository(CONF)
-    packages_to_update = CONF.get('upgrade_versions').get('packages') or PACKAGES
+    packages_to_update = CONF.get('update_versions').get('packages') or PACKAGES
     distro = distro_utils.get_distro(
         CONF.get('common').get('distro_name'),
         CONF.get('common').get('distro_version'),
         CONF.get('common').get('arch_and_endianness'))
     commit_updates = CONF.get('common').get('commit_updates')
     push_updates = CONF.get('common').get('push_updates')
-    push_repo_url = CONF.get('upgrade_versions').get('push_repo_url')
-    push_repo_branch = CONF.get('upgrade_versions').get('push_repo_branch')
+    push_repo_url = CONF.get('update_versions').get('push_repo_url')
+    push_repo_branch = CONF.get('update_versions').get('push_repo_branch')
     updater_name = CONF.get('common').get('updater_name')
     updater_email = CONF.get('common').get('updater_email')
 

config.yaml
@@ -84,8 +84,8 @@ build_iso:
         updates: "http://mirror.centos.org/altarch/7/updates/ppc64le/"
         extras: "http://mirror.centos.org/altarch/7/extras/ppc64le/"
         epel: "http://download.fedoraproject.org/pub/epel/7/ppc64le/"
-upgrade_versions:
-    packages:
+update_versions:
+ packages:
  push_repo_url:
  push_repo_branch: "master"
 update_versions_readme:

host_os.py
@@ -25,8 +25,8 @@ from lib.utils import is_package_installed
 from tools import build_iso
 from tools import build_packages
 from tools import build_release_notes
+from tools import update_versions
 from tools import update_versions_in_readme
-from tools import upgrade_versions
 
 INSUFFICIENT_PRIVILEGE_ERROR = 3
 TOO_MUCH_PRIVILEGE_ERROR = 4
@@ -36,7 +36,7 @@ LOG = logging.getLogger(__name__)
 SUBCOMMANDS = {
     'build-packages': build_packages,
     'build-release-notes': build_release_notes,
-    'upgrade-versions': upgrade_versions,
+    'update-versions': update_versions,
     'update-versions-readme': update_versions_in_readme,
     'build-iso': build_iso,
 }

lib/config.py
@@ -119,7 +119,7 @@ SUBCOMMANDS = [
         [PACKAGE_ARGS, PACKAGE_BUILD_ARGS, MOCK_ARGS, DISTRO_ARGS, BUILD_REPO_ARGS, BUILD_ARGS]),
     ('build-release-notes', 'Create release notes',
         [RELEASE_NOTES_ARGS, PUSH_REPO_ARGS, DISTRO_ARGS, BUILD_REPO_ARGS]),
-    ('upgrade-versions', 'Upgrade packages versions',
+    ('update-versions', 'Update packages versions',
         [PACKAGE_ARGS, PUSH_REPO_ARGS, DISTRO_ARGS, BUILD_REPO_ARGS]),
     ('update-versions-readme', 'Update the supported software versions table',
         [PUSH_REPO_ARGS, DISTRO_ARGS, BUILD_REPO_ARGS]),
@@ -262,7 +262,7 @@ class ConfigParser(object):
             "build-packages": "build_packages",
             "build-iso": "build_iso",
             "build-release-notes": "build_release_notes",
-            "upgrade-versions": "upgrade_versions",
+            "update-versions": "update_versions",
             "update-versions-readme": "update_versions_readme"
         }
         if command_line_args.subcommand in COMMAND_TO_CONFIG_NODE:

tests/unit/config_tests.py
@@ -26,8 +26,8 @@ class TestConfigParser(unittest.TestCase):
         (['build-release-notes', '--updater-email=foo'], 'updater_email', 'foo'),
         (['build-iso', '--packages-dir=foo'], 'packages_dir', 'foo'),
         (['build-iso', '--mock-args=foo'], 'mock_args', 'foo'),
-        (['upgrade-versions', '--no-commit-updates'], 'commit_updates', False),
-        (['upgrade-versions', '--no-push-updates'], 'push_updates', False),
+        (['update-versions', '--no-commit-updates'], 'commit_updates', False),
+        (['update-versions', '--no-push-updates'], 'push_updates', False),
     ])
     def test_parse_arguments_list_WithLongArgument_ShouldParseArgumentValue(self, arguments, key, expected):
         cfg = ConfigParser()
@@ -53,8 +53,8 @@ class TestConfigParser(unittest.TestCase):
         (['build-release-notes'], 'updater_email', None),
         (['build-iso'], 'packages_dir', 'result/packages/latest'),
         (['build-iso'], 'mock_args', ''),
-        (['upgrade-versions'], 'commit_updates', True),
-        (['upgrade-versions'], 'push_updates', True),
+        (['update-versions'], 'commit_updates', True),
+        (['update-versions'], 'push_updates', True),
     ])
     def test_parse_arguments_list_WithoutArgument_ShouldUseDefaultValue(self, arguments, key, expected):
         cfg = ConfigParser()

commit 2a8454d5be3ce76a2611acf8b217170b16e90fbc

Fix "update_versions" options
Rename them from "upgrade_versions".
Regression introduced by commit 5a2524a81a65cedd6a3731ac1a82793eaad9e10b

tools/update_versions.py
@@ -188,8 +188,8 @@ def run(CONF):
 
     REQUIRED_PARAMETERS = [("common", "updater_name"), ("common", "updater_email")]
     if push_updates:
-        REQUIRED_PARAMETERS += [("upgrade_versions", "push_repo_url"),
-                                ("upgrade_versions", "push_repo_branch")]
+        REQUIRED_PARAMETERS += [("update_versions", "push_repo_url"),
+                                ("update_versions", "push_repo_branch")]
     for section, parameter in REQUIRED_PARAMETERS:
         if CONF.get(section).get(parameter) is None:
             raise exception.RequiredParameterMissing(parameter=parameter)