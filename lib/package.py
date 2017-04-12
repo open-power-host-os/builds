open-power-host-os/builds

commit add1e7be82c7724ad6d21c8b6af46d09a7d10863

Use a different versions repository path for each subcommand
This allows different commands to be executed concurrently in the same
system. Executing the same command in parallel will still cause issues.

lib/versions_repository.py
@@ -22,8 +22,10 @@ def setup_versions_repository(config):
                         REPOSITORIES_DIR)
     url = config.get('common').get('packages_metadata_repo_url')
     branch = config.get('common').get('packages_metadata_repo_branch')
+    name = "versions_{subcommand}".format(
+        subcommand=config.get('common').get('subcommand'))
     try:
-        versions_repo = repository.get_git_repository(url, path)
+        versions_repo = repository.get_git_repository(url, path, name)
         versions_repo.checkout(branch)
     except exception.RepositoryError:
         LOG.error("Failed to checkout versions repository")

lib/config.py
@@ -324,6 +324,7 @@ class ConfigParser(object):
                     args.pop(key)
 
         config['common'].update(args)
+        config['common']['subcommand'] = command_line_args.subcommand
         self._CONF = config
         return config
 

commit 2fdff0ae3a75d29ff3674c0bfdfb8f74ceba07b6

Fix versions repository paths
It was being obtained from the basename of the URL in some places, which
is no longer true.

Create function to get versions repository.

Regression introduced in commit add1e7be82c7724ad6d21c8b6af46d09a7d10863

lib/package.py
@@ -25,6 +25,7 @@ from lib import exception
 from lib import package_source
 from lib import repository
 from lib import utils
+from lib.versions_repository import get_versions_repository
 from lib.constants import REPOSITORIES_DIR
 
 CONF = config.get_config().CONF
@@ -82,14 +83,11 @@ class Package(object):
         # versions of package metadata. This keeps compatibility.
         OLD_DEPENDENCIES_DIRS = ["build_dependencies", "dependencies"]
         PACKAGES_DIRS = [""] + OLD_DEPENDENCIES_DIRS
-        versions_repo_url = CONF.get('common').get('packages_metadata_repo_url')
-        versions_repo_name = os.path.basename(os.path.splitext(versions_repo_url)[0])
-        versions_repo_target_path = os.path.join(
-            PACKAGES_REPOS_TARGET_PATH,
-            versions_repo_name)
+        versions_repo = get_versions_repository(CONF)
+
         for rel_packages_dir in PACKAGES_DIRS:
             packages_dir = os.path.join(
-                versions_repo_target_path, rel_packages_dir)
+                versions_repo.working_tree_dir, rel_packages_dir)
             package_dir = os.path.join(packages_dir, self.name)
             package_file = os.path.join(package_dir, self.name + ".yaml")
             if os.path.isfile(package_file):

lib/packages_manager.py
@@ -18,8 +18,8 @@ import os
 
 from lib import config
 from lib import exception
-from lib.constants import REPOSITORIES_DIR
 from lib.package import Package
+from lib.versions_repository import get_versions_repository
 
 CONF = config.get_config().CONF
 LOG = logging.getLogger(__name__)
@@ -72,11 +72,7 @@ def discover_packages():
     "kernel" and "libvirt" will be discovered, "not-a-package" and "file"
     will not.
     """
-    config = CONF.get('common')
-    versions_repo_url = config.get('packages_metadata_repo_url')
-    versions_repo_name = os.path.basename(os.path.splitext(versions_repo_url)[0])
-    versions_repo_target_path = os.path.join(config.get('work_dir'),
-        REPOSITORIES_DIR, versions_repo_name)
+    versions_repo_target_path = get_versions_repository(CONF).working_tree_dir
     package_list = []
     try:
         package_list = [

lib/versions_repository.py
@@ -9,19 +9,46 @@ from lib.constants import REPOSITORIES_DIR
 LOG = logging.getLogger(__name__)
 
 
-def setup_versions_repository(config):
+def get_versions_repository(config):
     """
-    Clone and checkout the packages metadata git repository and halt execution if
-    anything fails.
+    Get the packages metadata Git repository, cloning it if does not
+    yet exist.
+
+    Args:
+        config (dict): configuration dictionary
+
+    Raises:
+        exception.RepositoryError: if the clone is unsuccessful
     """
     path = os.path.join(config.get('common').get('work_dir'),
                         REPOSITORIES_DIR)
     url = config.get('common').get('packages_metadata_repo_url')
-    branch = config.get('common').get('packages_metadata_repo_branch')
     name = "versions_{subcommand}".format(
         subcommand=config.get('common').get('subcommand'))
     try:
         versions_repo = repository.get_git_repository(url, path, name)
+    except exception.RepositoryError:
+        LOG.error("Failed to clone versions repository")
+        raise
+
+    return versions_repo
+
+
+def setup_versions_repository(config):
+    """
+    Prepare the packages metadata Git repository, cloning it and
+    checking out at the chosen branch.
+
+    Args:
+        config (dict): configuration dictionary
+
+    Raises:
+        exception.RepositoryError: if the clone or checkout are
+            unsuccessful
+    """
+    versions_repo = get_versions_repository(config)
+    branch = config.get('common').get('packages_metadata_repo_branch')
+    try:
         versions_repo.checkout(branch)
     except exception.RepositoryError:
         LOG.error("Failed to checkout versions repository")