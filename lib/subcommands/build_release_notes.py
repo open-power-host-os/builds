open-power-host-os/builds

commit 70750c1337a8c8d1d84baf47b84a5c7587cf3f11

Make empty values in config/host_os.yaml have correct types
It will be easier to treat them this way in the following patches.

lib/subcommands/build_release_notes.py
@@ -115,7 +115,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter) is None:
+        if CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     LOG.info("Creating release notes with packages: {}".format(

lib/subcommands/update_metapackage.py
@@ -45,7 +45,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter) is None:
+        if CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     METAPACKAGE_NAME = "open-power-host-os"

lib/subcommands/update_versions.py
@@ -209,7 +209,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter) is None:
+        if CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     # get packages names

lib/subcommands/update_versions_in_readme.py
@@ -44,7 +44,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter) is None:
+        if CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     update_versions_in_readme(versions_repo, distro, packages)

config/host_os.yaml
@@ -16,10 +16,10 @@ common:
  pungi_binary: "/usr/bin/pungi"
  commit_updates: True
  push_updates: True
- updater_name:
- updater_email:
+ updater_name: ''
+ updater_email: ''
 build_packages:
- packages:
+ packages: []
  keep_build_dir: False
  force_rebuild: False
  mock_config:
@@ -36,7 +36,7 @@ build_packages:
 build_release_notes:
  release_notes_repo_url: "https://github.com/open-power-host-os/open-power-host-os.github.io.git"
  release_notes_repo_branch: "master"
- push_repo_url:
+ push_repo_url: ''
  push_repo_branch: "master"
 build_iso:
     iso_name: "OpenPOWER-Host_OS"
@@ -82,12 +82,12 @@ build_iso:
         extras: "http://mirror.centos.org/altarch/7/extras/ppc64le/"
         epel: "http://download.fedoraproject.org/pub/epel/7/ppc64le/"
 update_metapackage:
- push_repo_url:
+ push_repo_url: ''
  push_repo_branch: "master"
 update_versions:
- packages:
- push_repo_url:
+ packages: []
+ push_repo_url: ''
  push_repo_branch: "master"
 update_versions_readme:
- push_repo_url:
+ push_repo_url: ''
  push_repo_branch: "master"

tests/unit/config_tests.py
@@ -41,7 +41,7 @@ class TestConfigParser(unittest.TestCase):
         (['build-packages'], 'verbose', False),
         (['build-packages'], 'work_dir', 'workspace'),
         (['build-packages'], 'keep_build_dir', False),
-        (['build-packages'], 'packages', None),
+        (['build-packages'], 'packages', []),
         (['build-packages'], 'result_dir', 'result'),
         (['build-packages'], 'packages_metadata_repo_url',
          'https://github.com/open-power-host-os/versions.git'),
@@ -50,10 +50,10 @@ class TestConfigParser(unittest.TestCase):
          '--plugin-option=tmpfs:keep_mounted=True --plugin-option='
          'tmpfs:max_fs_size=32g --plugin-option=tmpfs:required_ram_mb=39800 '
          '--verbose'),
-        (['build-release-notes'], 'push_repo_url', None),
+        (['build-release-notes'], 'push_repo_url', ''),
         (['build-release-notes'], 'push_repo_branch', 'master'),
-        (['build-release-notes'], 'updater_name', None),
-        (['build-release-notes'], 'updater_email', None),
+        (['build-release-notes'], 'updater_name', ''),
+        (['build-release-notes'], 'updater_email', ''),
         (['build-iso'], 'packages_dir', 'result/packages/latest'),
         (['build-iso'], 'mock_args', '--enable-plugin=tmpfs '
          '--plugin-option=tmpfs:keep_mounted=True --plugin-option='

commit 0aaa58f2aec53645b64b0afdf0e2b431a22de1c2

Fix exception condition with missing parameters
Regression introduced in commit 70750c1337a8c8d1d84baf47b84a5c7587cf3f11

lib/subcommands/build_release_notes.py
@@ -115,7 +115,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter):
+        if not CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     LOG.info("Creating release notes with packages: {}".format(

lib/subcommands/update_metapackage.py
@@ -45,7 +45,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter):
+        if not CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     METAPACKAGE_NAME = "open-power-host-os"

lib/subcommands/update_versions.py
@@ -209,7 +209,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter):
+        if not CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     # get packages names

lib/subcommands/update_versions_in_readme.py
@@ -44,7 +44,7 @@ def run(CONF):
     if push_updates:
         REQUIRED_PARAMETERS += ["push_repo_url", "push_repo_branch" ]
     for parameter in REQUIRED_PARAMETERS:
-        if CONF.get(parameter):
+        if not CONF.get(parameter):
             raise exception.RequiredParameterMissing(parameter=parameter)
 
     update_versions_in_readme(versions_repo, distro, packages)